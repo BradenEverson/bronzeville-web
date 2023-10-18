d2b482a60fd5bd3fd42d5af1a2aa55293e79233a
= <=>
2023-10-18 10:12:05 -0500

    Added project documentation :)))

--- handlers.go
+++ handlers.go
@@ -5,11 +5,12 @@ import (
 	"net/http"
 )
 
+//Return main index file
 func mainReq(w http.ResponseWriter, r *http.Request){
     tmpl := template.Must(template.ParseFiles("web/index.html"))
     tmpl.Execute(w, nil)
 }
-
+//Return static styles css file
 func getStyles(w http.ResponseWriter, r *http.Request){
     tmpl := template.Must(template.ParseFiles("web/styles/main.css"))
     tmpl.Execute(w, nil)
--- main.go
+++ main.go
@@ -6,10 +6,14 @@ import (
 )
 
 func main(){
-    
+    //Http route section
+    //Each http.HandleFunc method takes in the route along with the function used to return the htmx needed
+    //Handler functions are found at the handlers.go file on the root directory
     http.HandleFunc("/", mainReq)
     http.HandleFunc("/styles", getStyles)
 
+    //Set up the actual http server and listen on a specified port
+    //TODO dynamically change port so that heroku can automatically hand one over :)
     log.Println("Now listening on port 8000 B)")
     log.Fatal(http.ListenAndServe(":8000", nil))
 }
